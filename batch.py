bea159ba5ae6f85720ba6065204b22022602a676
wltan <wltan@users.noreply.github.com>
2020-01-19 01:13:03 +0800

    Add batch mode code

---
+++ batch.py
@@ -0,0 +1,14 @@
+def launch(src, stdin, lang, on_finish, on_close):
+    """
+    Parameters
+    src (str) - path to source code file
+    stdin (str) - path to input file (value is None if no file supplied)
+    lang (str) - language
+    on_finish (unary function) - call when program has successfully terminated; 
+                                argument is the path to output file
+    on_close (nullary function) - call when container is dead
+    """
+    pass
+
+def kill(instance):
+    pass
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from telegram import *
 import logging
 import sys
 import re
+import os
 
 from api_key import API_KEY
 import repl
+import batch
 
 # Command handlers
 def start(update, context):
@@ -37,7 +39,12 @@ def mode(update, context):
         # 2. Batch mode
         drop_data(update, context)
         context.chat_data["mode"] = 2
-        update.message.reply_text("Set mode 2")
+        update.message.reply_text("Please upload your source files. Recognized file types:\n" +
+                                ".txt - Will be sent to standard input\n" + 
+                                ".c - gcc\n" +
+                                ".cpp - g++\n" +
+                                ".java - JDK\n" +
+                                ".py - CPython")
     else:
         update.message.reply_text("Please use one of the following:\n" +
                                   "'/mode 1' - REPL mode\n" +
@@ -48,16 +55,80 @@ def exit(update, context):
     Kills any currently running container instances.
     """
     if "container" in context.chat_data:
-        repl.kill(context.chat_data["container"])
+        if context.chat_data["mode"] == 1:
+            repl.kill(context.chat_data["container"])
+        if context.chat_data["mode"] == 2:
+            batch.kill(context.chat_data["container"])
+        update.message.reply_text("Container terminated")
     else:
         update.message.reply_text("Error: Interpreter not started or already terminated")
 
+def run(update, context):
+    """
+    Runs the batch process specified for mode 2
+    """
+    if "mode" in context.chat_data and context.chat_data["mode"] == 2:
+        if "file_ext" in context.chat_data:
+            ext = context.chat_data["file_ext"]
+            chat_id = str(update.effective_chat.id)
+            src = "user_data/" + chat_id + ext
+            stdin = "user_data/" + chat_id + ".txt"
+            if not os.path.exists("user_data/" + chat_id + ".txt"):
+                stdin = None
+            lang = {
+                ".c": "c",
+                ".cpp": "c++",
+                ".java": "java",
+                ".py": "python"
+            }[ext]
+            def on_finish(outfile):
+                doc = open(outfile, "r")
+                update.message.reply_document(document=doc)
+            def on_close():
+                context.chat_data.pop("container", None)
+            batch.launch(src, stdin, lang, on_finish, on_close)
+        else:
+            update.message.reply_text("Error: No source code file detected.")
+    else:
+        update.message.reply_text("Error: This command is only available in mode 2. Please run /mode to change the mode.")
+
+# Message handlers
 def default(update, context):
+    """
+    Called on any noncommand text message
+    In mode 1, pipes the message into the container if it exists.
+    """
     if "mode" in context.chat_data and context.chat_data["mode"] == 1:
         if "container" in context.chat_data:
             repl.pipein(context.chat_data["container"], update.message.text + "\n")
         else:
             update.message.reply_text("Error: Interpreter not started or already terminated")
+    else:
+        pass # no defined behaviour in other modes
+
+KNOWN_FILE_TYPES = (".txt", ".c", ".cpp", ".java", ".py")
+def document(update, context):
+    """
+    Called on any message with a document
+    In mode 2, stores and prepares the files for compilation in the container
+    """
+    if "mode" in context.chat_data and context.chat_data["mode"] == 2:
+        doc = update.message.document
+        file_ext = os.path.splitext(doc.file_name)[1]
+        if file_ext in KNOWN_FILE_TYPES:
+            chat_id = str(update.effective_chat.id)
+            if file_ext != ".txt":
+                context.chat_data["file_ext"] = file_ext
+                # delete previous files
+                for other in KNOWN_FILE_TYPES[1:]:
+                    path = "user_files/" + chat_id + other
+                    if os.path.exists(path):
+                        os.remove(path)
+            doc.get_file().download(custom_path="user_files/" + chat_id + file_ext)
+        else:
+            update.message.reply_text("Unknown file type: " + file_ext)
+    else:
+        pass # no defined behaviour in other modes
 
 # Callback handlers
 def button(update, context):
@@ -90,6 +161,11 @@ def drop_data(update, context):
     """
     if "container" in context.chat_data:
         repl.kill(context.chat_data["container"])
+    chat_id = str(update.effective_chat.id)
+    for ext in KNOWN_FILE_TYPES:
+        path = "user_files/" + chat_id + ext
+        if os.path.exists(path):
+            os.remove(path)
     context.user_data["mode"] = 0
     update.message.reply_text("Existing data cleared!")
 
@@ -108,13 +184,15 @@ def main():
 
     dp = updater.dispatcher
 
-    # Add command handlers
+    # Add handlers
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("mode", mode))
     dp.add_handler(CommandHandler("exit", exit))
-    dp.add_handler(MessageHandler(Filters.text, default))
+    dp.add_handler(CommandHandler("run", run))
 
-    # Add callback handler
+    dp.add_handler(MessageHandler(Filters.text, default))
+    dp.add_handler(MessageHandler(Filters.document, document))
+
     dp.add_handler(CallbackQueryHandler(button))
     
     updater.start_polling()
