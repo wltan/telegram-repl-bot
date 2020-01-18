b88a4e9a963915a74d4b6dbeac4248e344455c79
Ian Yong <ianyongyc@gmail.com>
2020-01-18 21:36:24 +0800

    Add Python REPL

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 docker build -t base -f dockerfiles/base-dockerfile .
 docker build -t source -f dockerfiles/source-dockerfile .
 docker build -t java -f dockerfiles/java-dockerfile .
+docker build -t python -f dockerfiles/python-dockerfile .
