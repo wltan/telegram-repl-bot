4727e8581c1066bdbc4cb2e3b1d1c3850817a5b2
Ian Yong <ianyongyc@gmail.com>
2020-01-18 16:05:29 +0800

    Add docker container for Source REPL

--- repl.py
+++ repl.py
@@ -1,22 +1,37 @@
-def launch(lang, pipeout):
-    """
-    Spawns a container with the interpreter for the given language.
-    Returns an instance of the container.
+import docker
+import time
 
-    pipeout is a function that takes a string and sends it back to the user.
-    Use it to send standard output from the container.
-    """
-    pass
+class Repl:
+    def __init__(self):
+        self.client = docker.from_env()
+        self.container = None
 
-def pipein(container, text):
-    """
-    Sends the text string into the container as standard input.
-    There is no need to return anything.
-    """
-    pass
+    def launch(self, lang, pipeout):
+        """
+        Spawns a container with the interpreter for the given language.
+        Returns an instance of the container.
 
-def kill(container):
-    """
-    Stops the container.
-    """
-    pass
+        pipeout is a function that takes a string and sends it back to the user.
+        Use it to send standard output from the container.
+        """
+        if lang == "source":
+            self.container = self.client.containers.run("source", "1", stdin_open = True, tty = True, detach = True)
+
+    def pipein(self, text):
+        """
+        Sends the text string into the container as standard input.
+        There is no need to return anything.
+        """
+        pass
+
+    def kill(self):
+        """
+        Stops the container.
+        """
+        self.container.stop()
+
+# For debugging
+repl = Repl()
+repl.launch("source", 1)
+time.sleep(5)
+repl.kill()
---
+++ setup.sh
@@ -0,0 +1,2 @@
+docker build -t base -f dockerfiles/base-dockerfile .
+docker build -t source -f dockerfiles/source-dockerfile .
