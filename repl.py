fd084359ae6ff2acc88a4a30c270f2270e9ea53a
Ian Yong <ianyongyc@gmail.com>
2020-01-18 17:47:39 +0800

    Attach pipeout to stdout of container
    Removed debug code in repl.py

--- repl.py
+++ repl.py
@@ -3,6 +3,7 @@ import time
 import threading
 
 POLL_INTERVAL = 0.5
+MESSAGE_LIMIT = 4096
 
 class Repl:
     def __init__(self):
@@ -28,8 +29,9 @@ class Repl:
         self.input = self.client.attach_socket(self.container, params={'stdin': 1, 'stream': 1})._sock
         self.output = self.client.attach_socket(self.container, params={'stdout': 1, 'stream': 1})._sock
 
+        # Initialise listener
         self.is_listening = True
-        self.listener = threading.Thread(target = self.__listen)
+        self.listener = threading.Thread(target = self.__listen, args = [pipeout])
         self.listener.start()
 
     def pipein(self, text):
@@ -40,27 +42,19 @@ class Repl:
         self.input.send(text.encode('utf-8')) # Convert to bytes
 
     def kill(self):
-        """
-        Stops the container.
-        """
-        self.client.stop(self.container)
-        self.client.remove_container(self.container)
+        self.stop_listener()
+        self.client.stop(self.container) # Stop the container
+        self.client.remove_container(self.container) # Remove the container
 
-    def __listen(self):
+    def __listen(self, pipeout):
         while True:
             if not self.is_listening:
                 break
-            print(self.output.recv(1024))
+            s = self.output.recv(MESSAGE_LIMIT)
+            if s != "":
+                pipeout(s)
             time.sleep(POLL_INTERVAL)
     
     def stop_listener(self):
         self.is_listening = False
-        self.listener.join()
-
-
-# For debugging
-repl = Repl()
-repl.launch("source", 1)
-repl.pipein("5 === 5;")
-time.sleep(5)
-repl.kill()
+        self.listener.join() # Wait for __listen to break
