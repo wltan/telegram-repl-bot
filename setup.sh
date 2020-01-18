28c46862f2987effcd9ebfdac64b34fa28a66e97
Ian Yong <ianyongyc@gmail.com>
2020-01-19 02:13:33 +0800

    Refactor dockerfiles

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 docker build -t base -f dockerfiles/base-dockerfile .
-docker build -t source -f dockerfiles/source-dockerfile .
+docker build -t source-repl -f dockerfiles/source-repl-dockerfile .
 docker build -t java -f dockerfiles/java-dockerfile .
+docker build -t java-repl -f dockerfiles/java-repl-dockerfile .
 docker build -t python -f dockerfiles/python-dockerfile .
+docker build -t python-repl -f dockerfiles/python-repl-dockerfile .
+docker build -t c-repl -f dockerfiles/c-repl-dockerfile .
